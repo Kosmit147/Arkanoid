84ac594a90b095d6ea1ea3b2762ed595db24b6c9
Wojciech Opara <wwojtek660@gmail.com>
2024-03-05 20:51:15 +0100

    Fix typo

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@ int main()
         return -1;
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     window = glfwCreateWindow(800, 600, "Arkanoid", NULL, NULL);
