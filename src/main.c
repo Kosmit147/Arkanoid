2ab00eb9bc822eacb838a9d94785223f04e2f952
Wojciech Opara <wwojtek660@gmail.com>
2024-03-08 10:12:50 +0100

    Add necessary defines for Apple platforms

--- src/main.c
+++ src/main.c
@@ -18,6 +18,10 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
     window = glfwCreateWindow(800, 600, "Arkanoid", NULL, NULL);
 
     if (!window)
