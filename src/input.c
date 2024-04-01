fd4f7b34f668db119e3c06a74d0ebb470354d89f
Wiktor <74769437+Wikierk@users.noreply.github.com>
2024-04-01 22:46:29 +0200

    made limit to paddle movement

--- src/input.c
+++ src/input.c
@@ -1,7 +1,6 @@
-#include "input.h"
-
 #include <stdbool.h>
 
+#include "input.h"
 #include "defines.h"
 
 extern float deltaTime;
@@ -28,9 +27,16 @@ void moveBall(Ball* ball, Block* paddle, GLFWwindow* window)
 
 void movePaddle(Block* paddle, GLFWwindow* window)
 {
+    if (paddle->position.x <= 0.f)
+        paddle->position.x = 0.f;
+
+    if (paddle->position.x + paddle->width >= COORDINATE_SPACE)
+        paddle->position.x = COORDINATE_SPACE - paddle->width;
+
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         paddle->position.x += PADDLE_SPEED * deltaTime;
 
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
         paddle->position.x -= PADDLE_SPEED * deltaTime;
+
 }
