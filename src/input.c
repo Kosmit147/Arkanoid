0e7003cdb6fc59d3bdb163221be79f36ddbc5dfd
Wiktor <74769437+Wikierk@users.noreply.github.com>
2024-04-13 16:29:58 +0200

    fix if issue

--- src/input.c
+++ src/input.c
@@ -27,16 +27,18 @@ void moveBall(Ball* ball, Block* paddle, GLFWwindow* window)
 
 void movePaddle(Block* paddle, GLFWwindow* window)
 {
-    if (paddle->position.x <= 0.f)
-        paddle->position.x = 0.f;
-
-    if (paddle->position.x + paddle->width >= COORDINATE_SPACE)
-        paddle->position.x = COORDINATE_SPACE - paddle->width;
 
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         paddle->position.x += PADDLE_SPEED * deltaTime;
 
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
         paddle->position.x -= PADDLE_SPEED * deltaTime;
+
+    if (paddle->position.x <= 0.f)
+        paddle->position.x = 0.f;
+
+    else if (paddle->position.x + paddle->width >= COORDINATE_SPACE)
+        paddle->position.x = COORDINATE_SPACE - paddle->width;
+
 
 }
