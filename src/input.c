0fed60cc1a65a42f20e06c13e83575ecbe9d1d6d
Wojciech Opara <wwojtek660@gmail.com>
2024-04-15 19:19:01 +0200

    Refactor

--- src/input.c
+++ src/input.c
@@ -13,8 +13,8 @@ static void movePaddle(Block* paddle, GLFWwindow* window)
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
         paddle->position.x -= PADDLE_SPEED * deltaTime;
 
-    if (paddle->position.x < 0.f)
-        paddle->position.x = 0.f;
+    if (paddle->position.x < 0.0f)
+        paddle->position.x = 0.0f;
     else if (paddle->position.x + paddle->width > COORDINATE_SPACE)
         paddle->position.x = COORDINATE_SPACE - paddle->width;
 }
