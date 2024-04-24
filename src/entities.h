c06d6782b598536fb1e4549a387d9fb68cc17148
Wojciech Opara <wwojtek660@gmail.com>
2024-04-25 01:29:13 +0200

    Use compound literals

--- src/entities.h
+++ src/entities.h
@@ -13,19 +13,17 @@ typedef struct Block
 typedef struct Ball
 {
     Vec2 position; // ball center
+    float radius;
     Vec2 direction;
     float speed;
-    float radius;
 } Ball;
 
 static inline Vec2 getClosestPointOnBlock(const Ball* ball, const Block* block)
 {
-    Vec2 result = {
+    return (Vec2) {
         .x = clamp(block->position.x, block->position.x + block->width, ball->position.x),
         .y = clamp(block->position.y - block->height, block->position.y, ball->position.y),
     };
-
-    return result;
 }
 
 static inline void reflectBall(Ball* ball, Vec2 normal)
