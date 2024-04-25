7b4bbf8dd9e2b84d592e8fa67d36d52a60b42bcf
Wojciech Opara <wwojtek660@gmail.com>
2024-04-25 17:09:53 +0200

    Separate game rendering and opengl-specific code

--- src/rendering.c
+++ src/rendering.c
@@ -1,15 +1,14 @@
 #include "rendering.h"
 
+#include <glad/glad.h>
+
 #define INCBIN_PREFIX
 #include <incbin.h>
 
+#include <stddef.h>
 #include <stdlib.h>
 #include <assert.h>
-#include <math.h>
 
-#include "helpers.h"
-#include "log.h"
-#include "board.h"
 #include "shader.h"
 
 #include "defines.h"
@@ -21,142 +20,6 @@ INCTXT(paddleVertexShaderSrc, "../shaders/paddle.vert");
 INCTXT(paddleFragmentShaderSrc, "../shaders/paddle.frag");
 INCTXT(ballVertexShaderSrc, "../shaders/ball.vert");
 INCTXT(ballFragmentShaderSrc, "../shaders/ball.frag");
-
-#ifdef _DEBUG
-void rendererGLDebugCallback(GLenum unused(source), GLenum unused(type), GLuint unused(id), GLenum severity, 
-    GLsizei unused(length), const GLchar* message, const void* unused(userParam))
-{
-    if (ARKANOID_GL_DEBUG_MESSAGE_MIN_SEVERITY != GL_DEBUG_SEVERITY_NOTIFICATION &&
-        severity > ARKANOID_GL_DEBUG_MESSAGE_MIN_SEVERITY)
-    {
-        return;
-    }
-
-    switch (severity)
-    {
-    case GL_DEBUG_SEVERITY_NOTIFICATION:
-        logNotification("[OpenGL Notification]: %s.\n", message);
-        break;
-    case GL_DEBUG_SEVERITY_LOW:
-    case GL_DEBUG_SEVERITY_MEDIUM:
-        logWarning("[OpenGL Warning]: %s.\n", message);
-        break;
-    case GL_DEBUG_SEVERITY_HIGH:
-        logError("[OpenGL Error]: %s.\n", message);
-        break;
-    }
-}
-#endif
-
-unsigned int genVA()
-{
-    unsigned int VA;
-    glGenVertexArrays(1, &VA);
-    glBindVertexArray(VA);
-
-    return VA;
-}
-
-unsigned int genVB()
-{
-    unsigned int VB;
-    glGenBuffers(1, &VB);
-    glBindBuffer(GL_ARRAY_BUFFER, VB);
-
-    return VB;
-}
-
-unsigned int genIB()
-{
-    unsigned int IB;
-    glGenBuffers(1, &IB);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IB);
-
-    return IB;
-}
-
-int retrieveUniformLocation(unsigned int shader, const char* name)
-{
-    int location = glGetUniformLocation(shader, name);
-
-#ifdef _DEBUG
-    if (location == -1)
-        logWarning("[OpenGL Warning]: Uniform %s in shader %d does not exist!\n", name, shader);
-#endif
-
-    return location;
-}
-
-void drawVertices(unsigned int VA, int count, GLenum IBType)
-{
-    glBindVertexArray(VA);
-    glDrawElements(GL_TRIANGLES, count, IBType, NULL);
-}
-
-void moveDataWithinGLBuffer(GLenum bufferType, unsigned int buffer, GLintptr dstOffset,
-    GLintptr srcOffset, GLsizeiptr size)
-{
-    void* tmpData = malloc((size_t)size);
-
-    glBindBuffer(bufferType, buffer);
-    glGetBufferSubData(bufferType, srcOffset, size, tmpData);
-    glBufferSubData(bufferType, dstOffset, size, tmpData);
-
-    free(tmpData);
-}
-
-void moveObjectsWithinGLBuffer(GLenum bufferType, unsigned int buffer, size_t dstIndex,
-    size_t srcIndex, size_t count, size_t objSize)
-{
-    GLintptr srcOffset = (GLintptr)(objSize * srcIndex);
-    GLintptr dstOffset = (GLintptr)(objSize * dstIndex);
-    GLsizeiptr dataSize = (GLsizeiptr)(objSize * count);
-
-    moveDataWithinGLBuffer(bufferType, buffer, dstOffset, srcOffset, dataSize);
-}
-
-void eraseObjectFromGLBuffer(GLenum bufferType, unsigned int buffer, size_t index, size_t objectCount, size_t objSize)
-{
-    size_t objectsToMove = objectCount - index - 1;
-    moveObjectsWithinGLBuffer(bufferType, buffer, index, index + 1, objectsToMove, objSize);
-}
-
-unsigned int createQuadIB(size_t count, GLenum usage)
-{
-    unsigned int IB = genIB();
-
-    size_t dataSize = sizeof(unsigned short) * 2 * 3 * count;
-    unsigned short* indices = malloc(dataSize);
-
-    unsigned short vertexOffset = 0;
-
-    for (unsigned short i = 0; i < count * 6; i += 6)
-    {
-        // first triangle
-        indices[i + 0] = vertexOffset + 0;
-        indices[i + 1] = vertexOffset + 1;
-        indices[i + 2] = vertexOffset + 2;
-
-        // second triangle
-        indices[i + 3] = vertexOffset + 0;
-        indices[i + 4] = vertexOffset + 2;
-        indices[i + 5] = vertexOffset + 3;
-
-        vertexOffset += 4;
-    }
-
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, (GLsizei)dataSize, indices, usage);
-
-    free(indices);
-
-    return IB;
-}
-
-void freeGLQuad(const GLQuad* quad)
-{
-    glDeleteVertexArrays(1, &quad->VA);
-    glDeleteBuffers(1, &quad->VB);
-}
 
 static void getBlockVertices(float* vertices, const Block* block)
 {
@@ -384,7 +247,8 @@ void freeRenderingData(const GameRenderingData* renderingData)
 
 static void updateBallShaderUnifs(const BallShaderUnifs* unifs, const Ball* ball)
 {
-    glUniform2f(unifs->normalBallCenter, normalizeCoordinate(ball->position.x), normalizeCoordinate(ball->position.y));
+    glUniform2f(unifs->normalBallCenter, normalizeCoordinate(ball->position.x),
+        normalizeCoordinate(ball->position.y));
     glUniform1f(unifs->normalBallRadiusSquared, powf(normalizeLength(ball->radius), 2.0f));
 }
 
@@ -410,7 +274,7 @@ static void drawBlocks(size_t blockCount, unsigned int shader, unsigned int bloc
 void render(const GameRenderingData* renderingData, const GameObjects* gameObjects)
 {
     drawPaddle(renderingData->shaders.paddleShader, renderingData->paddleQuad.VA);
-    drawBlocks(gameObjects->blockCount, renderingData->shaders.blockShader, 
+    drawBlocks(gameObjects->blockCount, renderingData->shaders.blockShader,
         renderingData->blocksQuad.VA);
     drawBall(&gameObjects->ball, renderingData->shaders.ballShader,
         &renderingData->ballShaderUnifs, renderingData->ballQuad.VA);
--- src/rendering.h
+++ src/rendering.h
@@ -1,19 +1,7 @@
 #pragma once
 
-#include <glad/glad.h>
-
-#include <stddef.h>
-
-#include "entities.h"
+#include "gl.h"
 #include "board.h"
-
-#include "defines.h"
-
-typedef struct GLQuad
-{
-    // after adding new buffers update freeGLQuad()
-    unsigned int VA, VB;
-} GLQuad;
 
 typedef struct BallShaderUnifs
 {
@@ -39,29 +27,6 @@ typedef struct GameRenderingData
     GLQuad blocksQuad;
     GLQuad ballQuad;
 } GameRenderingData;
-
-#ifdef _DEBUG
-void rendererGLDebugCallback(GLenum source, GLenum type, GLuint id, GLenum severity,
-    GLsizei length, const GLchar* message, const void* userParam);
-#endif
-
-unsigned int genVA();
-unsigned int genVB();
-unsigned int genIB();
-
-int retrieveUniformLocation(unsigned int shader, const char* name);
-void drawVertices(unsigned int VA, int count, GLenum IBType);
-
-void moveDataWithinGLBuffer(GLenum bufferType, unsigned int buffer, GLintptr dstOffset,
-    GLintptr srcOffset, GLsizeiptr size);
-void moveObjectsWithinGLBuffer(GLenum bufferType, unsigned int buffer, size_t dstIndex,
-    size_t srcIndex, size_t count, size_t objSize);
-void eraseObjectFromGLBuffer(GLenum bufferType, unsigned int buffer, size_t index,
-    size_t objectCount, size_t objSize);
-
-unsigned int createQuadIB(size_t count, GLenum usage);
-
-void freeGLQuad(const GLQuad* quad);
 
 void initRenderingData(GameRenderingData* data, const GameObjects* gameObjects);
 void updateRenderingData(GameRenderingData* renderingData, const GameObjects* gameObjects);
