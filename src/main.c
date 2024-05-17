9709157fb9b42a921cd1b04000912fe0e2db0811
Wojciech Opara <wwojtek660@gmail.com>
2024-05-18 00:11:37 +0200

    Use defines for exit codes

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <stb_image.h>
 
 #include <time.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 #include "window.h"
@@ -22,10 +23,10 @@ int main()
     GLFWwindow* window = setUpWindow("Arkanoid", WINDOW_WIDTH, WINDOW_HEIGHT);
 
     if (!window)
-        return -1;
+        return EXIT_FAILURE;
 
     if (!loadGlad())
-        return -1;
+        return EXIT_FAILURE;
 
     initGLViewport(window);
     glfwSetFramebufferSizeCallback(window, onWindowResize);
@@ -89,5 +90,5 @@ int main()
     freeGame(&game);
 
     glfwTerminate();
-    return 0;
+    return EXIT_SUCCESS;
 }
